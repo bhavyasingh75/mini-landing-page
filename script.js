f72c0b386a9d90f03bde63bce185cfacb1c47832
Bhavya Singh <102183018+bhavyasingh75@users.noreply.github.com>
2023-06-29 16:16:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// javascript
+var button = document.getElementById("submit-button");
+
+//click listener
+button.addEventListener('click', function(){
+    alert("Thanks for stopping by!!! You'll receive a mail shortly.");
+});
